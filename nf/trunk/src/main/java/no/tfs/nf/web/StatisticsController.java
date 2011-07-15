9db189467b0f5c65743764481b6cf3647f2ab0a2
Lars Helge Overland <larshelge@gmail.com>
2011-07-15 13:38:42 +0200

    m

--- nf/trunk/src/main/java/no/tfs/nf/web/StatisticsController.java
+++ nf/trunk/src/main/java/no/tfs/nf/web/StatisticsController.java
@@ -3,6 +3,7 @@ package no.tfs.nf.web;
 import no.tfs.nf.api.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,7 @@ public class StatisticsController
     private CategoryService categoryService;
     
     @RequestMapping("/statistics")
+    @Secured({"ROLE_MANAGER"})
     public ModelAndView getStatistics()
     {
         return new ModelAndView( "maintenance/statistics" ).addObject( "statistics", categoryService.getStatistics() );
