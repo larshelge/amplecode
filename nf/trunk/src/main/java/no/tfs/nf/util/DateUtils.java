4722a1663b3126ca4ed0ae01b9a2462d86bc6f2b
Lars Helge Overland <larshelge@gmail.com>
2011-10-02 16:33:20 +0200

    Minor fix

--- nf/trunk/src/main/java/no/tfs/nf/util/DateUtils.java
+++ nf/trunk/src/main/java/no/tfs/nf/util/DateUtils.java
@@ -6,23 +6,28 @@ import java.util.Date;
 
 public class DateUtils
 {
-    private static final SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd" );
+    private static final SimpleDateFormat SHORT_FORMAT = new SimpleDateFormat( "yyyy-MM-dd" );
+    private static final SimpleDateFormat LONG_FORMAT = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
     
     public static String getDateString( Date date )
     {
-        return format.format( date );
+        return SHORT_FORMAT.format( date );
+    }
+
+    public static String getLongDateString( Date date )
+    {
+        return LONG_FORMAT.format( date );
     }
     
     public static Date getDate( String string )
     {
         try
         {
-            return format.parse( string );
+            return SHORT_FORMAT.parse( string );
         }
         catch ( ParseException ex )
         {
             throw new RuntimeException( "Failed to parse medium date", ex );
         }
     }
-
 }
